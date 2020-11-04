d1a2c30deb6a2ce4c00f0555298e9c6ab4f2911b
MatthewARussell <matthewrussell904@hotmail.com>
2020-11-04 12:45:23 -0500

    1 file modified

--- js/add-element.js
+++ js/add-element.js
@@ -3,23 +3,27 @@
  */
 
 // get all the hot class elements- change them to cool class
-$("li.hot").removeClass("hot");
-$("li").addClass("cool");
+$('li.hot').removeClass('hot');
+$('li').addClass('cool');
 
 // traverse the elements
-$("#one")
-  .next()
-  .next()
-  .text("milk");
+$("one").next().next().text("milk");
 
 // add a new element
 
-// add by clicking the plus sign
-$("add").click(addElement);
 
+// add by clicking the plus sign
+
+$('add').click(addElement);
 function addElement() {
   // add a new element
-  if ($(this).hasClass("complete")) {
+  
+}
+
+
+//  click the li element will change the changeStyle
+function changeStyle() {
+if ($(this).hasClass("complete")) {
     $(this).removeClass("complete");
     $(this).addClass("cool");
   }
@@ -35,15 +39,11 @@ function addElement() {
   }
 }
 
-//  click the li element will change the changeStyle
-function changeStyle() {
-  if ($(this).hasClass("complete")) {
-  }
-}
-
 // delete by clicking the trash can
 document.getElementById("remove").addEventListener("click", removeElement);
 
 function removeElement() {
   // remove the marked element
+ 
+ 
 }
